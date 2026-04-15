a4b62c764225d81d8be18e788195dcac7b5504e4
devopthay <thay.hale@gmail.com>
2026-04-14 23:40:03 -0300

    Adicionando menu inicial da aplicação

--- reading_list.py
+++ reading_list.py
@@ -1,1 +1,7 @@
-print("Lista de leitura iniciada")
+print("Lista de leitura")
+
+print("1 - Adicionar livro")
+print("2 - Listar livros")
+print("3 - Marcar como lido")
+print("4 - Remover livro")
+print("5 - Sair")
