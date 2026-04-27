ee81568b6e2425062cacf74a97443150109bb415
devopthay <thay.hale@gmail.com>
2026-04-26 23:31:16 -0300

    Update reading_list.py

--- reading_list.py
+++ reading_list.py
@@ -1,16 +1,29 @@
-print("Lista de leitura")
+class ReadingList:
+    def __init__(self):
+        self.books = []
 
-livros = []
+    def add_book(self, book):
+        self.books.append(book)
 
-print("1 - Adicionar livro")
-print("2 - Listar livros")
-print("3 - Marcar como lido")
-print("4 - Remover livro")
-print("5 - Sair")
+    def remove_book(self, book):
+        if book in self.books:
+            self.books.remove(book)
 
-opcao = input("Escolha uma opção: ")
 
-if opcao == "1":
-    livro = input("Digite o nome do livro: ")
-    livros.append(livro)
-    print("Livro adicionado com sucesso!")
+if __name__ == "__main__":
+    print("Lista de leitura")
+
+    livros = []
+
+    print("1 - Adicionar livro")
+    print("2 - Listar livros")
+    print("3 - Marcar como lido")
+    print("4 - Remover livro")
+    print("5 - Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        livro = input("Digite o nome do livro: ")
+        livros.append(livro)
+        print("Livro adicionado com sucesso!")
